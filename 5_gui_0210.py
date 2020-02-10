8dd0c22cc24b8a6e824c3b899043ea3881cc4777
Elwing-Chou <birdfan8814@gmail.com>
2020-02-10 14:04:02 +0800

    Add files via upload

--- 5_gui_0210.py
+++ 5_gui_0210.py
@@ -1,5 +1,7 @@
 # 創造Frame -> 創造元件(Frame) -> 元件.pack()
 import tkinter as tk
+import threading
+import time
 from jieba.analyse import extract_tags
 
 class JiebaFrame(tk.Frame):
@@ -14,14 +16,21 @@ class JiebaFrame(tk.Frame):
         self.t1.pack()
         self.b1 = tk.Button(self,
                             text="請點我分析",
-                            command=self.analyse)
+                            command=self.analyse_thread)
         self.b1.pack(expand=True, fill=tk.BOTH)
         self.result = tk.Label(self, text="按上面按鈕得到關鍵詞")
         self.result.pack()
 
+    def analyse_thread(self):
+        thread = threading.Thread(target=self.analyse)
+        thread.start()
+
     def analyse(self):
+        self.b1["state"] = tk.DISABLED
         text = self.t1.get("1.0", "end")
+        time.sleep(5)
         self.result["text"] = str(extract_tags(text, 5))
+        self.b1["state"] = tk.ACTIVE
 
 window = tk.Tk()
 window.geometry("500x500+200+200")
